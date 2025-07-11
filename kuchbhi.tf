be1d08dcfb1b05a288ef7cae12ed4ce3ddfb5ca4
Your Name <shahedali2001>
2025-07-11 18:53:31 +0530

    provider

--- kuchbhi.tf
+++ kuchbhi.tf
@@ -8,3 +8,12 @@ ncbchc'bscheck "ssscheck "dfssscheck "name" {
   name = "example"
   value = "exagti"
 }
+
+terraform {
+  {require_provider=
+ {provider= /harshicorp/azurerm
+ veriosn=1.3.4
+ }
+
+}
+}
