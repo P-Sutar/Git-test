939447bcf8f22c8a4269525144d4d4831c1662dc
Jayadev <jayadev.das@ashilsurana99gmail.onmicrosoft.com>
2025-07-11 19:04:17 +0530

    code added

--- kuchbhi.tf
+++ kuchbhi.tf
@@ -8,3 +8,9 @@ ncbchc'bscheck "ssscheck "dfssscheck "name" {
   name = "example"
   value = "exagti"
 }
+
+# PS D:\DevOps_Insider_Training\Terraform_codes\New folder\Git-test> git checkout -b feature/jd-001
+# Switched to a new branch 'feature/jd-001'
+# PS D:\DevOps_Insider_Training\Terraform_codes\New folder\Git-test> git branch
+# * feature/jd-001
+#   main
